5a948abfb0ef4ee34270a3a956b96fa21b2eaf0c
leegyuseung <lgs4002@naver.com>
2022-09-25 02:15:16 +0900

    더미 데이터 생성하기

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,43 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2022-09-25"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2022-09-28"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2022-10-01"),
+  },
+  {
+    id: "e4",
+    description: "A Book",
+    amount: 14.99,
+    date: new Date("2022-10-05"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.93,
+    date: new Date("2022-10-07"),
+  },
+];
+
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
